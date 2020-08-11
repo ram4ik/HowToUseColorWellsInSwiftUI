b0cc155c18b4f0bf0ba45f672b6d42b116ed7354
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-11 15:12:29 +0300

    Initial Commit

---
+++ HowToUseColorWellsInSwiftUI/HowToUseColorWellsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseColorWellsInSwiftUIApp.swift
+//  HowToUseColorWellsInSwiftUI
+//
+//  Created by ramil on 11.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseColorWellsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
